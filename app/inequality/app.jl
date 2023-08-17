8caa320ff42237ae8ef5e930ad039b37d301052d
Stef Kuypers <stef@happonomy.org>
2023-08-17 15:47:19 +0200

    Removed use of Currency type

--- app/inequality/app.jl
+++ app/inequality/app.jl
@@ -28,33 +28,37 @@ end
     @in sim_model::Symbol = :Standard
 
     # Standard model parameters
-    @in initial_wealth::Currency = Currency(1000.0)
+    @in initial_wealth = 1000
 
     # SuMSy parameters
-    @in guaranteed_income::Currency = Currency(2000.0)
+    @in guaranteed_income::Int = 2000
     @in dem_percentages = [1.0]
-    @in dem_tiers = [Currency(25000)]
+    @in dem_tiers = [25000]
     @in add_dem_tier = false
     @in remove_dem_tier = false
 
     @onchange add_dem_tier begin
         @show dem_percentages
-        push!(dem_percentages, last(dem_percentages) + 1)
+        last_percentage = isempty(dem_percentages) ? 1.0 : dem_percentages[end] + 1
+        push!(dem_percentages, last_percentage)
         dem_percentages = copy(dem_percentages)
 
-        # @show dem_tiers
-        # push!(dem_tiers, last(dem_tiers) + 1000)
-        # dem_tiers = copy(dem_tiers)
+        @show dem_tiers
+        last_tier = isempty(dem_tiers) ? 25000 : dem_tiers[end] + 1000
+        push!(dem_tiers, last_tier)
+        dem_tiers = copy(dem_tiers)
     end
 
     @onchange remove_dem_tier begin
-        @show dem_percentages
-        pop!(dem_percentages)
-        dem_percentages = copy(dem_percentages)
+        if !isempty(dem_tiers)
+            @show dem_percentages
+            pop!(dem_percentages)
+            dem_percentages = copy(dem_percentages)
 
-        # @show dem_tiers
-        # pop!(dem_tiers)
-        # dem_tiers = copy(dem_tiers)
+            @show dem_tiers
+            pop!(dem_tiers)
+            dem_tiers = copy(dem_tiers)
+        end
     end
 
     # Debt Based parameters
